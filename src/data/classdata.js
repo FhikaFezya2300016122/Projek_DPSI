22bee577fd115df482f310d25794b5d908e62a46
Kanzacpp <caraclik96@gmail.com>
2025-06-25 15:21:36 +0700

    update : remove folder data

--- src/data/classdata.js
+++
@@ -1,8 +0,0 @@
-// src/data/classData.js
-
-// Kita export database ini agar bisa diimpor di file lain
-export const FAKE_CLASS_DATABASE = {
-    'CODE7C': { id: 'C7', name: 'Class 7C', title: 'English Learning', teacher: 'Salsabila', imageUrl: 'https://images.unsplash.com/photo-1543286386-713bdd548da4?q=80&w=2070&auto=format&fit=crop'},
-    'CODE8B': { id: 'B8', name: 'Class 8B', title: 'Creative Writing', teacher: 'Khomarudin', imageUrl: 'https://images.unsplash.com/photo-1491841550275-5b462bf48569?q=80&w=2070&auto=format&fit=crop'},
-    'CODE9A': { id: 'A9', name: 'Class 9A', title: 'Public Speaking', teacher: 'Elizabeth', imageUrl: 'https://images.unsplash.com/photo-1524995997946-a1c2e315a42f?q=80&w=2070&auto=format&fit=crop'},
-};
